54ee388e79b8748d78fa47bc920a73ae04c22886
furkanoksuz99 <148237754+furkanoksuz99@users.noreply.github.com>
2024-08-13 13:26:41 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿
+namespace Soru1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridOzluk = new System.Windows.Forms.DataGridView();
+            this.gridDers = new System.Windows.Forms.DataGridView();
+            this.gridNot = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTcNo = new System.Windows.Forms.TextBox();
+            this.txtAdi = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtDersAdi = new System.Windows.Forms.TextBox();
+            this.txtPuan = new System.Windows.Forms.TextBox();
+            this.BtnKaydet = new System.Windows.Forms.Button();
+            this.BtnSil = new System.Windows.Forms.Button();
+            this.BtnGuncelle = new System.Windows.Forms.Button();
+            this.dataGrid1 = new System.Windows.Forms.DataGrid();
+            ((System.ComponentModel.ISupportInitialize)(this.gridOzluk)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridDers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridNot)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridOzluk
+            // 
+            this.gridOzluk.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridOzluk.Location = new System.Drawing.Point(438, 12);
+            this.gridOzluk.Name = "gridOzluk";
+            this.gridOzluk.Size = new System.Drawing.Size(350, 150);
+            this.gridOzluk.TabIndex = 0;
+            // 
+            // gridDers
+            // 
+            this.gridDers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridDers.Location = new System.Drawing.Point(438, 168);
+            this.gridDers.Name = "gridDers";
+            this.gridDers.Size = new System.Drawing.Size(350, 150);
+            this.gridDers.TabIndex = 1;
+            // 
+            // gridNot
+            // 
+            this.gridNot.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridNot.Location = new System.Drawing.Point(438, 324);
+            this.gridNot.Name = "gridNot";
+            this.gridNot.Size = new System.Drawing.Size(350, 150);
+            this.gridNot.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(74, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tc Kimlik";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(74, 92);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(28, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Adı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(74, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Soyadı";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(74, 186);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Ders Adı";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(76, 231);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(39, 16);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Puan";
+            // 
+            // txtTcNo
+            // 
+            this.txtTcNo.Location = new System.Drawing.Point(77, 55);
+            this.txtTcNo.MaxLength = 11;
+            this.txtTcNo.Name = "txtTcNo";
+            this.txtTcNo.Size = new System.Drawing.Size(207, 20);
+            this.txtTcNo.TabIndex = 8;
+            // 
+            // txtAdi
+            // 
+            this.txtAdi.Location = new System.Drawing.Point(77, 111);
+            this.txtAdi.Name = "txtAdi";
+            this.txtAdi.Size = new System.Drawing.Size(207, 20);
+            this.txtAdi.TabIndex = 9;
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(77, 163);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(207, 20);
+            this.txtSoyad.TabIndex = 10;
+            // 
+            // txtDersAdi
+            // 
+            this.txtDersAdi.Location = new System.Drawing.Point(77, 208);
+            this.txtDersAdi.Name = "txtDersAdi";
+            this.txtDersAdi.Size = new System.Drawing.Size(207, 20);
+            this.txtDersAdi.TabIndex = 11;
+            // 
+            // txtPuan
+            // 
+            this.txtPuan.Location = new System.Drawing.Point(77, 250);
+            this.txtPuan.Name = "txtPuan";
+            this.txtPuan.Size = new System.Drawing.Size(207, 20);
+            this.txtPuan.TabIndex = 12;
+            // 
+            // BtnKaydet
+            // 
+            this.BtnKaydet.Location = new System.Drawing.Point(114, 295);
+            this.BtnKaydet.Name = "BtnKaydet";
+            this.BtnKaydet.Size = new System.Drawing.Size(109, 41);
+            this.BtnKaydet.TabIndex = 13;
+            this.BtnKaydet.Text = "Kaydet";
+            this.BtnKaydet.UseVisualStyleBackColor = true;
+            this.BtnKaydet.Click += new System.EventHandler(this.BtnKaydet_Click);
+            // 
+            // BtnSil
+            // 
+            this.BtnSil.Location = new System.Drawing.Point(114, 342);
+            this.BtnSil.Name = "BtnSil";
+            this.BtnSil.Size = new System.Drawing.Size(109, 41);
+            this.BtnSil.TabIndex = 14;
+            this.BtnSil.Text = "Sil";
+            this.BtnSil.UseVisualStyleBackColor = true;
+            this.BtnSil.Click += new System.EventHandler(this.BtnSil_Click);
+            // 
+            // BtnGuncelle
+            // 
+            this.BtnGuncelle.Location = new System.Drawing.Point(114, 389);
+            this.BtnGuncelle.Name = "BtnGuncelle";
+            this.BtnGuncelle.Size = new System.Drawing.Size(109, 41);
+            this.BtnGuncelle.TabIndex = 15;
+            this.BtnGuncelle.Text = "Güncelle";
+            this.BtnGuncelle.UseVisualStyleBackColor = true;
+            this.BtnGuncelle.Click += new System.EventHandler(this.BtnGuncelle_Click);
+            // 
+            // dataGrid1
+            // 
+            this.dataGrid1.DataMember = "";
+            this.dataGrid1.HeaderForeColor = System.Drawing.SystemColors.ControlText;
+            this.dataGrid1.Location = new System.Drawing.Point(18, 525);
+            this.dataGrid1.Name = "dataGrid1";
+            this.dataGrid1.Size = new System.Drawing.Size(763, 156);
+            this.dataGrid1.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(800, 693);
+            this.Controls.Add(this.dataGrid1);
+            this.Controls.Add(this.BtnGuncelle);
+            this.Controls.Add(this.BtnSil);
+            this.Controls.Add(this.BtnKaydet);
+            this.Controls.Add(this.txtPuan);
+            this.Controls.Add(this.txtDersAdi);
+            this.Controls.Add(this.txtSoyad);
+            this.Controls.Add(this.txtAdi);
+            this.Controls.Add(this.txtTcNo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.gridNot);
+            this.Controls.Add(this.gridDers);
+            this.Controls.Add(this.gridOzluk);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Soru 1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gridOzluk)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridDers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridNot)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gridOzluk;
+        private System.Windows.Forms.DataGridView gridDers;
+        private System.Windows.Forms.DataGridView gridNot;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTcNo;
+        private System.Windows.Forms.TextBox txtAdi;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtDersAdi;
+        private System.Windows.Forms.TextBox txtPuan;
+        private System.Windows.Forms.Button BtnKaydet;
+        private System.Windows.Forms.Button BtnSil;
+        private System.Windows.Forms.Button BtnGuncelle;
+        private System.Windows.Forms.DataGrid dataGrid1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace StudentInformationManagementSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void GridleriDoldur()
+        {
+            gridOzluk.DataSource = SQL.SelectCalistir("SELECT * FROM TBL_OZLUK");
+            gridDers.DataSource = SQL.SelectCalistir("SELECT * FROM TBL_DERS");
+            gridNot.DataSource = SQL.SelectCalistir("SELECT * FROM TBL_Not");
+        }
+
+        private void Temizle()
+        {
+            txtTcNo.Clear();
+            txtAdi.Clear();
+            txtSoyad.Clear();
+            txtDersAdi.Clear();
+            txtPuan.Clear();
+        }
+
+        private void Bagla()
+        {
+            DataSet ds = new DataSet();
+
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM TBL_OZLUK", SQL.baglanti);
+            SqlDataAdapter da2 = new SqlDataAdapter("SELECT * FROM TBL_DERS", SQL.baglanti);
+            SqlDataAdapter da3 = new SqlDataAdapter("SELECT * FROM TBL_Not", SQL.baglanti);
+
+            DataTable dt = new DataTable("TBL_OZLUK");
+            da.Fill(dt);
+            DataTable dt2 = new DataTable("TBL_DERS");
+            da2.Fill(dt2);
+            DataTable dt3 = new DataTable("TBL_Not");
+            da3.Fill(dt3);
+
+            ds.Tables.Add(dt);
+            ds.Tables.Add(dt2);
+            ds.Tables.Add(dt3);
+
+            DataRelation dr = new DataRelation("baglanti", dt.Columns["TcKimlik"], dt2.Columns["Tckimlik"]);
+            DataRelation dr2 = new DataRelation("baglanti2", dt2.Columns["TcKimlik"], dt3.Columns["TcKimlik"]);
+
+            ds.Relations.Add(dr);
+            ds.Relations.Add(dr2);
+
+            dataGrid1.DataSource = ds;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GridleriDoldur();
+            Bagla();
+        }
+
+        private void BtnKaydet_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (SQL.InsertUpdateDeleteCalistir("INSERT INTO TBL_OZLUK VALUES ('" + txtTcNo.Text + "','" + txtAdi.Text + "','" + txtSoyad.Text + "')"))
+                {
+                    if (SQL.InsertUpdateDeleteCalistir("INSERT INTO TBL_DERS VALUES ('" + txtTcNo.Text + "','" + txtDersAdi.Text + "')"))
+                    {
+                        if (SQL.InsertUpdateDeleteCalistir("INSERT INTO TBL_Not VALUES ('" + txtTcNo.Text + "'," + Convert.ToDecimal(txtPuan.Text) + ")"))
+                        {
+                            MessageBox.Show("Kaydetme işlemi başarılı", "Bilgi Mesajı", MessageBoxButtons.OK);
+                            GridleriDoldur();
+                            Temizle();
+                        }
+                        else
+                        {
+                            SQL.InsertUpdateDeleteCalistir("DELETE FROM TBL_OZLUK WHERE TcKimlik='" + txtTcNo.Text + "'");
+                            SQL.InsertUpdateDeleteCalistir("DELETE FROM TBL_DERS WHERE TcKimlik='" + txtTcNo.Text + "'");
+                        }
+                    }
+                    else
+                    {
+                        SQL.InsertUpdateDeleteCalistir("DELETE FROM TBL_OZLUK WHERE TcKimlik='" + txtTcNo.Text + "'");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("HATA:" + ex.Message.ToString(), "Hata Mesajı", MessageBoxButtons.OK);
+            }
+        }
+
+        private void BtnSil_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtTcNo.Text != "")
+                {
+                    DataTable dataTable;
+                    string adi, soyadi, dersAdi;
+
+                    dataTable = SQL.SelectCalistir("SELECT * FROM TBL_OZLUK WHERE TcKimlik='" + txtTcNo.Text + "'");
+                    adi = dataTable.Rows[0]["Adi"].ToString();
+                    soyadi = dataTable.Rows[0]["Soyad"].ToString();
+
+                    dataTable = SQL.SelectCalistir("SELECT * FROM TBL_DERS WHERE TcKimlik='" + txtTcNo.Text + "'");
+                    dersAdi = dataTable.Rows[0]["Ders_Adi"].ToString();
+
+                    if (SQL.InsertUpdateDeleteCalistir("DELETE FROM TBL_OZLUK WHERE TcKimlik='" + txtTcNo.Text + "'"))
+                    {
+                        if (SQL.InsertUpdateDeleteCalistir("DELETE FROM TBL_DERS WHERE TcKimlik='" + txtTcNo.Text + "'"))
+                        {
+                            if (SQL.InsertUpdateDeleteCalistir("DELETE FROM TBL_Not WHERE TcKimlik='" + txtTcNo.Text + "'"))
+                            {
+                                MessageBox.Show("Silme işlemi başarılı", "Bilgi Mesajı", MessageBoxButtons.OK);
+                                GridleriDoldur();
+                                Temizle();
+                            }
+                            else
+                            {
+                                SQL.InsertUpdateDeleteCalistir("INSERT INTO TBL_OZLUK VALUES ('" + txtTcNo.Text + "','" + adi + "','" + soyadi + "')");
+                                SQL.InsertUpdateDeleteCalistir("INSERT INTO TBL_DERS VALUES ('" + txtTcNo.Text + "','" + dersAdi + "')");
+                            }
+                        }
+                        else
+                        {
+                            SQL.InsertUpdateDeleteCalistir("INSERT INTO TBL_OZLUK VALUES ('" + txtTcNo.Text + "','" + adi + "','" + soyadi + "')");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Tc Kimlik alanı boş geçilemez", "Hata Mesajı", MessageBoxButtons.OK);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("HATA:" + ex.Message.ToString(), "Hata Mesajı", MessageBoxButtons.OK);
+            }
+        }
+
+        private void BtnGuncelle_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtTcNo.Text != "")
+                {
+                    if (SQL.InsertUpdateDeleteCalistir("UPDATE TBL_OZLUK SET Adi='" + txtAdi.Text + "',Soyad='" + txtSoyad.Text + "' WHERE TcKimlik='" + txtTcNo.Text + "'"))
+                    {
+                        if (SQL.InsertUpdateDeleteCalistir("UPDATE TBL_DERS SET Ders_Adi = '" + txtDersAdi.Text + "' WHERE TcKimlik='" + txtTcNo.Text + "'"))
+                        {
+                            if (SQL.InsertUpdateDeleteCalistir("UPDATE TBL_Not SET Puan = " + Convert.ToDecimal(txtPuan.Text) + " WHERE TcKimlik='" + txtTcNo.Text + "'"))
+                            {
+                                MessageBox.Show("Güncelleme işlemi başarılı", "Bilgi Mesajı", MessageBoxButtons.OK);
+                                GridleriDoldur();
+                                Temizle();
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Tc Kimlik alanı boş geçilemez", "Hata Mesajı", MessageBoxButtons.OK);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("HATA:" + ex.Message.ToString(), "Hata Mesajı", MessageBoxButtons.OK);
+            }
+        }
+    }
+}
